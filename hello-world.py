1e39a5229a429e5196161bd9d97167aed9aa5456
Justin.Whitaker <whitaker.jd@gmail.com>
2017-09-15 17:33:12 -0700

    Added hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Index!"
+
+
+@app.route("/hello/<string:name>/")
+def hello(name):
+    return render_template('profile.html',name=name)
+
+
+if __name__ == "__main__":
+    app.run()
